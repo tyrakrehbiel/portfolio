3ebb34b9af45a4d2982b1b799668fa5569e4f7ad
Tyra Krehbiel <43380449+tyrakrehbiel@users.noreply.github.com>
2020-09-10 16:14:35 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <CssBaseline />
+      {/* The rest of your application */}
+      <h1>Hello!</h1>
+    </>
+  );
+}
+
+export default App;
