9456d4c02f34d28a2e0163bb307c49aa7493222c
Tyra Krehbiel <43380449+tyrakrehbiel@users.noreply.github.com>
2020-09-11 11:08:28 -0400

    fixed formatting of new pages

--- src/features/art/Art.tsx
+++ src/features/art/Art.tsx
@@ -1,9 +1,25 @@
-import { Typography } from '@material-ui/core';
+import { createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
 import * as React from 'react';
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexgrow: 1,
+        },
+        
+        title: {
+            padding: "40px",
+            textAlign: "center",
+        },
+    })
+);
+
 const Art: React.FC = () => {
+
+    const classes = useStyles();
+
     return(
-        <Typography variant="h1">Art Page | Coming Soon!</Typography>
+        <Typography variant="h1" className={classes.title}>Art Page | Coming Soon!</Typography>
     )
 }
 
--- src/features/contact/Contact.tsx
+++ src/features/contact/Contact.tsx
@@ -1,9 +1,25 @@
-import { Typography } from '@material-ui/core';
+import { createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
 import * as React from 'react';
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexgrow: 1,
+        },
+        
+        title: {
+            padding: "40px",
+            textAlign: "center",
+        },
+    })
+);
+
 const Contact: React.FC = () => {
+
+    const classes = useStyles();
+
     return(
-        <Typography variant="h1">Contact Page | Coming Soon!</Typography>
+        <Typography variant="h1" className={classes.title}>Contact Page | Coming Soon!</Typography>
     )
 }
 
