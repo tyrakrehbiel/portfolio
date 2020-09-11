a6d5444f100b0881b308b8bc70a60b175400c6a9
Tyra Krehbiel <43380449+tyrakrehbiel@users.noreply.github.com>
2020-09-11 09:13:04 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tkrehbielportfolio/precache-manifest.c07254205bcebf26c6403b848e97feef.js"
+  "/portfolio/precache-manifest.b2678e4a4b64cab12ddafcbc3481678d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tkrehbielportfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
