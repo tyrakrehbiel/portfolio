b5865bdeabec21fec60e3d45e9399b2a7534ac37
Tyra Krehbiel <43380449+tyrakrehbiel@users.noreply.github.com>
2020-09-11 00:21:43 -0400

    pdfs not working

--- src/features/resume/Resume.tsx
+++ src/features/resume/Resume.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
+import { Document, Page } from 'react-pdf';
 
 const Resume: React.FC = () => {
     return(
-        <h1>Resume Page</h1>
+        <>
+            <h1>Resume Page</h1>
+            <Document file='./f20_resume.pdf'>
+                <Page pageNumber={1} />
+            </Document>
+        </>
     )
 }
 
